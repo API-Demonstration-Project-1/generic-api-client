5b55ba7b896e0a0cb73df94177850d3bed1ae827
PrvSunKS <48259084+PrvSunKS@users.noreply.github.com>
2020-04-19 15:34:33 +0530

    First Commit
    First Commit

---
+++ src/main/java/com/toystore/ecomm/clientapp/controller/ClientAppController.java
@@ -0,0 +1,24 @@
+package com.toystore.ecomm.clientapp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.toystore.ecomm.UserManagementServiceDiscoveryClient;
+import com.toystore.ecomm.clientapp.model.User;
+
+@RestController
+@RequestMapping(value="test")
+public class ClientAppController {
+	
+	@Autowired
+	UserManagementServiceDiscoveryClient client;
+	
+	@RequestMapping(value="/{userId}",method = RequestMethod.GET)
+	public User getUserById(@PathVariable("userId") String userId) {
+		return client.getUser(userId);
+	}
+
+}
